b4222a06e93eb3a455133a3b51e6089226983a00
mikas <6666350@gmail.com>
2023-01-19 19:32:53 +0100

    adds routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {ProfilePageComponent} from "./page/profile-page/profile-page.component";
+import {HomePageComponent} from "./page/home-page/home-page.component";
+import {IssuePageComponent} from "./page/issue-page/issue-page.component";
 
-
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'profile', component: ProfilePageComponent},
+  {path: 'myIssues', component: IssuePageComponent},
+  {path: 'issue', component: IssuePageComponent},
+  {path: 'favorites', component: ProfilePageComponent},
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/component/menu-bottom/menu-bottom.component.ts
+++ src/app/component/menu-bottom/menu-bottom.component.ts
@@ -6,5 +6,91 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bottom.component.css']
 })
 export class MenuBottomComponent {
+  colClass: string = "col";
+  linkClass = "text-dark small font-weight-bold text-decoration-none";
+  textDanger = "text-danger";
+  homeIconClass: string = "feather-home";
+  myIssuesIconClass: string = "feather-map-pin";
+  favoritesIconClass: string = "feather-heart";
+  profileIconClass: string = "feather-user";
 
+  homeColClass: string = this.colClass;
+  myIssuesColClass: string = this.colClass;
+  favoritesColClass: string = this.colClass;
+  profileColClass: string = this.colClass;
+
+  homeLinkClass: string = this.linkClass;
+  myIssuesLinkClass: string = this.linkClass;
+  favoritesLinkClass: string = this.linkClass;
+  profileLinkClass: string = this.linkClass;
+
+
+  ngOnInit(): void {
+    switch (window.location.pathname) {
+      case "/":
+        this.homeColClass = this.colClass + " selected";
+        this.myIssuesColClass = this.colClass;
+        this.favoritesColClass= this.colClass;
+        this.profileColClass = this.colClass;
+
+        this.homeLinkClass = this.linkClass.replaceAll("text-dark", this.textDanger);
+        this.myIssuesLinkClass = this.linkClass;
+        this.favoritesLinkClass = this.linkClass;
+        this.profileLinkClass = this.linkClass;
+
+        this.homeIconClass = "feather-home " + this.textDanger;
+        this.myIssuesIconClass = "feather-map-pin";
+        this.favoritesIconClass = "feather-heart";
+        this.profileIconClass = "feather-user";
+        break;
+      case "/profile":
+        this.homeColClass = this.colClass;
+        this.myIssuesColClass = this.colClass;
+        this.favoritesColClass= this.colClass;
+        this.profileColClass = this.colClass + " selected";
+
+        this.homeLinkClass = this.linkClass
+        this.myIssuesLinkClass = this.linkClass;
+        this.favoritesLinkClass = this.linkClass;
+        this.profileLinkClass = this.linkClass.replaceAll("text-dark", this.textDanger);
+
+        this.homeIconClass = "feather-home";
+        this.myIssuesIconClass = "feather-map-pin";
+        this.favoritesIconClass = "feather-heart";
+        this.profileIconClass = "feather-user " + this.textDanger;
+        break;
+      case "/favorites":
+        this.homeColClass = this.colClass;
+        this.myIssuesColClass = this.colClass;
+        this.favoritesColClass= this.colClass + " selected";
+        this.profileColClass = this.colClass;
+
+        this.homeLinkClass = this.linkClass
+        this.myIssuesLinkClass = this.linkClass;
+        this.favoritesLinkClass = this.linkClass.replaceAll("text-dark", this.textDanger);
+        this.profileLinkClass = this.linkClass;
+
+        this.homeIconClass = "feather-home";
+        this.myIssuesIconClass = "feather-map-pin";
+        this.favoritesIconClass = "feather-heart " + this.textDanger;
+        this.profileIconClass = "feather-user";
+        break;
+      case "/myIssues":
+        this.homeColClass = this.colClass;
+        this.myIssuesColClass = this.colClass + " selected";
+        this.favoritesColClass= this.colClass;
+        this.profileColClass = this.colClass;
+
+        this.homeLinkClass = this.linkClass
+        this.myIssuesLinkClass = this.linkClass.replaceAll("text-dark", this.textDanger);;
+        this.favoritesLinkClass = this.linkClass;
+        this.profileLinkClass = this.linkClass;
+
+        this.homeIconClass = "feather-home";
+        this.myIssuesIconClass = "feather-map-pin " + this.textDanger;
+        this.favoritesIconClass = "feather-heart";
+        this.profileIconClass = "feather-user";
+        break;
+    }
+  }
 }
