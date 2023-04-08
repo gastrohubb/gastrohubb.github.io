5370a808c1eb7909316e8be1d590a811f48e92a7
mikas <6666350@gmail.com>
2023-04-08 22:57:21 +0200

    changes mistype in keycloak url

--- src/app/init/keycloak-init.factory.ts
+++ src/app/init/keycloak-init.factory.ts
@@ -6,7 +6,7 @@ export function initializeKeycloak(
     return () =>
         keycloak.init({
             config: {
-                url: 'https://18.133.48.89:8443/' + '/auth',
+                url: 'https://18.133.48.89:8443' + '/auth',
                 realm: 'usterkafix',
                 clientId: 'gastrohubb.github.io'
             },
