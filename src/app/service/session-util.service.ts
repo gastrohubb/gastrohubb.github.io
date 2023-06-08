a1ca39a567c2f1c7690d9b6142218ca8243fde3e
Maksim Shelkovich <mikas@Maksims-MacBook-Pro.local>
2023-06-09 00:27:05 +0200

    [fix]: fixes stackOverFlow due to recursion when getUser from session

--- src/app/service/session-util.service.ts
+++ src/app/service/session-util.service.ts
@@ -5,6 +5,7 @@ import {KeycloakUser} from "../dto/KeycloakUser";
 import {catchError, throwError} from "rxjs";
 import {ConfigService} from "./config.service";
 import {HttpClient} from "@angular/common/http";
+import {KeycloakProfile} from "keycloak-js";
 
 @Injectable({
     providedIn: 'root'
@@ -22,13 +23,25 @@ export class SessionUtilService {
         if (userJson != null) {
             let user: GhbUser = GhbUser.fromJson(JSON.parse(userJson));
             return user;
-        } else if (this.requestForUserDataIsInProgress) {
-            setTimeout(() => this.getUser(), 1000);
-            return this.getUser();
         }
-        this.requestForUserDataIsInProgress = true;
-        this.getOrCreateUserAndPutInSession();
-        return this.getUser();
+        console.log('User is not is session, verify or create new');
+        this.getOrCreateUserAndSetUserToSession().then(user => {
+            this.putUser(user);
+        });
+        // todo: we need to wait when user will be in session instead of returning new GhbUser.
+        return new GhbUser();
+    }
+
+    public async getOrCreateUserAndSetUserToSession(): Promise<GhbUser> {
+        console.log('Taking userProfile from Keycloak');
+        const keycloakProfile: KeycloakProfile = await this.getUserKeycloakProfile();
+        console.log('Map it to keyckloakUser instance');
+        const keycloakUser: KeycloakUser = this.makeKeycloakUser(keycloakProfile);
+        console.log('Sending instance to server to verify it exist or create new');
+        const ghbUser: GhbUser = this.getOrCreateUser(keycloakUser);
+        console.log('Putting ghbUser instance to session');
+        this.setUserToSession(ghbUser);
+        return ghbUser;
     }
 
     public putUser(user: GhbUser) {
@@ -43,31 +56,53 @@ export class SessionUtilService {
         return userProfile.id;
     }
 
+    public async getUserKeycloakProfile(): Promise<KeycloakProfile> {
+        try {
+            const profile = await this.keycloak.loadUserProfile();
+            return profile;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    public makeKeycloakUser(keycloakProfile: KeycloakProfile) {
+        let keycloakUser: KeycloakUser = new KeycloakUser();
+        keycloakUser.setUserName(keycloakProfile.username);
+        keycloakUser.setEmail(keycloakProfile.email);
+        keycloakUser.setUserId(keycloakProfile.id);
+        return keycloakUser;
+    }
+
+    public getOrCreateUser(keycloakUser: KeycloakUser): GhbUser {
+        const url = this.config.apiUrl() + "/getOrCreateUser";
+        const data = JSON.stringify(keycloakUser);
+
+        const httpRequest = new XMLHttpRequest();
+        httpRequest.open("POST", url, false);
+        httpRequest.setRequestHeader("Content-Type", "application/json");
+        try {
+            httpRequest.send(data);
+            if (httpRequest.status === 200) {
+                const response: GhbUser = JSON.parse(httpRequest.responseText);
+                return response;
+            } else {
+                // Handle non-200 status code
+                console.error(`POST request failed with status: ${httpRequest.status}`);
+            }
+        } catch (error) {
+            // Handle error
+            console.error(error);
+        }
+        return new GhbUser();
+        // todo: consider pattern emptyObject|null|undefined
+    }
+    public setUserToSession(user: GhbUser) {
+        this.putUser(user);
+    }
+
     async isLoggedIn(): Promise<boolean> {
         const isLoggedIn = await this.keycloak.isLoggedIn();
         return isLoggedIn;
-    }
-
-    getOrCreateUserAndPutInSession(): void {
-        this.keycloak.loadUserProfile()
-            .then(userProfile => {
-                let keycloakUser: KeycloakUser = new KeycloakUser();
-                keycloakUser.setUserName(userProfile.username);
-                keycloakUser.setEmail(userProfile.email);
-                keycloakUser.setUserId(userProfile.id);
-                console.log(keycloakUser);
-                this.http.post<GhbUser>(this.config.apiUrl() + "/getOrCreateUser", keycloakUser)
-                    .pipe(
-                        catchError(error => {
-                            console.log(error)
-                            return throwError(() => new Error(error));
-                        }))
-                    .subscribe(u => {
-                            this.putUser(u);
-                            this.requestForUserDataIsInProgress = false;
-                        }
-                    )
-            })
-            .catch(error => console.log(error));
     }
 }
